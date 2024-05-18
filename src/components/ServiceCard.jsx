f649557fee62a5f16db29e22a19712bd8e65b461
MuhammadSamiAhmad <mrmidosami@yahoo.com>
2024-05-18 11:27:33 +0300

    Update ServiceCard.jsx

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,51 +1,60 @@
 import React from "react";
-import { Box, useTheme, Stack, Typography } from "@mui/material";
-import { tokens } from "../theme";
-import Image from "../assets/images/WhatsApp Image 2024-05-15 at 1.47.21 PM.jpeg";
+import { useTheme, Stack, Typography } from "@mui/material";
 
-const ServiceCard = ({ serviceName, serviceDescription }) => {
+const ServiceCard = ({ serviceName, serviceDescription, Image }) => {
   const theme = useTheme();
-  const colors = tokens(theme.palette.mode);
-  const colorStyle =
-    theme.palette.mode === "dark"
-      ? `${colors.blueAccent[700]}`
-      : "hsla(0,0%,82%,.3)";
   const shadowBorderColor = theme.palette.mode === "dark" ? "#868dfb" : "black";
 
   return (
-    <Stack
-      sx={{
-        minWidth: "450px",
-        maxWidth: "50vw",
-        width: "40vw",
-        height: "40vh",
-        maxHeight: "90vh",
-        backgroundColor: colorStyle,
-        p: "3% 4% 4% 4%",
-        boxShadow: `1px 1px 8px ${shadowBorderColor}`,
-        borderRadius: "10px",
-        ":hover": {
-          transition: "all 0.5s ease-out",
-          scale: "1.1",
-          opacity: "0.9",
-        },
-        marginLeft: "3%",
-        backgroundImage: `url(${Image})`,
-      }}
-    >
-      <Box>
-        <Stack display={"flex"} flexDirection={"column"} gap={3}>
-          <Stack display={"flex"} flexDirection={"column"} gap={4}>
-            <Typography variant="h2" fontWeight={"bold"}>
-              {serviceName}
-            </Typography>
-            <Typography variant="h5" fontSize={"20px"}>
-              {serviceDescription}
-            </Typography>
-          </Stack>
+    <>
+      <Stack
+        position={"relative"}
+        sx={{
+          minWidth: "450px",
+          maxWidth: "50vw",
+          width: "40vw",
+          height: "40vh",
+          maxHeight: "90vh",
+          p: "3% 4% 1% 1%",
+          boxShadow: `1px 1px 8px ${shadowBorderColor}`,
+          borderRadius: "10px",
+          ":hover": {
+            transition: "all 0.5s ease-out",
+            scale: "1.1",
+            opacity: "0.9",
+          },
+          marginLeft: "3%",
+          backgroundImage: `url(${Image})`,
+          backgroundSize: "cover",
+        }}
+        display={"flex"}
+        flexDirection={"column"}
+        justifyContent={"end"}
+        alignContent={"end"}
+      >
+        <Stack
+          sx={{
+            backgroundColor: "#ffffff",
+            opacity: " 0.8",
+            borderRadius: "10px",
+            padding: "1%",
+          }}
+          gap={3}
+        >
+          <Typography variant="h2" fontWeight={"bold"} color={"#36987f"}>
+            {serviceName}
+          </Typography>
+          <Typography
+            variant="h5"
+            fontSize={"20px"}
+            color={"#000"}
+            sx={{ opacity: "2" }}
+          >
+            {serviceDescription}
+          </Typography>
         </Stack>
-      </Box>
-    </Stack>
+      </Stack>
+    </>
   );
 };
 
